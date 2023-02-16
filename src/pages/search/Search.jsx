869b19de15766d295736dbc95ebf415e62f2885f
Satyabrat Ojha <satyabratojha04@gmail.com>
2023-02-16 22:30:35 +0530

    search

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,7 +1,24 @@
 import "./search.scss";
+import Analyze from "../../assets/Analyze.png";
+import { height } from "@mui/system";
 
 const Search = () => {
-  return <div>search</div>;
+  return (
+    <div className="search">
+      <div className="heading">
+        <h1>Reveal true POTENTIAL of the song</h1>
+        <h2>Zero discrimination over advertisements and investments</h2>
+      </div>
+      <div className="container">
+        <div className="search-box">
+          <input type="text" placeholder="The song in your mind" />
+          <button>
+            <img src={Analyze} alt="analyze" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
