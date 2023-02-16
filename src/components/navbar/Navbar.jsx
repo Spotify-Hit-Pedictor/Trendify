9cbac30e19f352dba4a4a45c026c0f1642395971
Satyabrat Ojha <satyabratojha04@gmail.com>
2023-02-16 12:53:26 +0530

    minor dev

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import "./navbar.scss";
 import Item from "./Item";
+import Logo from "../../assets/trendify_logo_bg.png";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="left">
+        <img src={Logo} alt="logo" className="logo" />
         <p className="title">TRENDIFY</p>
       </div>
       <div className="right">
